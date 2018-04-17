315b886cf22ce1639ca4635dd14e58e4c7852361
Thiago Henrique <thiagonascimento95@hotmail.com>
2018-04-16 23:47:16 -0300

    initial commit

---
+++ src/main/java/com/thiagosilva/demo/jasperreportspring/JasperreportSpringApplication.java
@@ -0,0 +1,12 @@
+package com.thiagosilva.demo.jasperreportspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperreportSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperreportSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/thiagosilva/demo/jasperreportspring/JasperreportSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thiagosilva.demo.jasperreportspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JasperreportSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
